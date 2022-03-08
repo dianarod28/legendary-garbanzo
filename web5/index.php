5d940be85584f1b4b5d510351ee004ad2549a9ed
dianarod28 <201880207@psu.palawan.edu.ph>
2022-03-08 08:29:31 +0800

    finished web 5

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,27 @@
+  
 <?php
+    session_start();
 
+    $num_visits = $_SESSION['visitCount'];
+    if (isset($_GET['reset']) || !isset($num_visits)){
+        $num_visits = 0;
+    }
+    $num_visits++;
+    $_SESSION['visitCount'] = $num_visits;
 ?>
 
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+<?php if ($num_visits >= 5): ?>
+    <h2> Wow! </h2>
+    <p> You sure come to this page a lot. </p>
+<?php endif; ?>
+
+<?php if ($num_visits>=10): ?>
+    <?php
+    header("Location: https://localhost/legendary-garbanzo/web5/congratulations.php");
+    exit();
+    ?>    
+<?php endif; ?>    
